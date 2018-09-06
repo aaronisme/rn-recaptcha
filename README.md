ba0bd6e2aedec113fcd60fbb3838302703ce1d17
Aaron Chen <chen_dong85@foxmail.com>
2018-09-06 22:27:09 +0800

    init the project

---
+++ README.md
@@ -0,0 +1,62 @@
+# rn-recaptcha
+this is the React native component for Google Recaptch V2
+
+## How to install
+```js
+ yarn rn-recaptcha
+ npm install rn-recaptcha
+```
+## How to use 
+you can clone this project and check the App.js for see How to use it. 
+or just like this.
+
+```js
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import RNRecaptcha from 'RNRecaptcha';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNRecaptcha siteKey={siteKey} onMessage={onMessage} url={testUrl}/>
+      </View>
+    );
+  }
+}
+
+const siteKey='6LcWuG4UAAAAAAn1v2RHkpyaE6yaXAc-uyRDSgMG';
+const testUrl = 'https://rnrecaptcha.org';
+
+onMessage = event => {
+  if ( event && event.nativeEvent.data) {
+    console.log(event.nativeEvent.data)
+  } else if(event.nativeEvent.data === 'expired'){
+    console.log('this is when you expired')
+  }else if(event.nativeEvent.data === 'error') {
+    console.log('this is when error')
+  } else {
+    console.log('other')
+  }
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20
+  },
+});
+```
+
+siteKey is the siteKey of the Google Recaptcha
+
+testUrl is the url Domain defined on your google Recaptcha
+
+onMessage is the callback function which will be called after the google verification.
+
+## Demo
+there is RN Project Demo you can check:
+[Demo](https://github.com/aaronisme/RNRecaptchaDemo)
+
+Any Suggestion or Questions, please contact me.
---
+++ index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { WebView } from 'react-native';
+import PropTypes from 'prop-types';
+
+// fix https://github.com/facebook/react-native/issues/10865
+const patchPostMessageJsCode = `(${String(function() {
+  var originalPostMessage = window.postMessage;
+  var patchedPostMessage = function(message, targetOrigin, transfer) {
+    originalPostMessage(message, targetOrigin, transfer);
+  };
+  patchedPostMessage.toString = function() {
+    return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+  };
+  window.postMessage = patchedPostMessage;
+})})();`;
+
+const generateTheWebViewContent = siteKey => {
+  const originalForm =
+    '<!DOCTYPE html><html><head>' +
+    '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+    '<script src="https://recaptcha.google.cn/recaptcha/api.js"></script>' +
+    '<script type="text/javascript"> var onloadCallback = function() { }; ' +
+    'var onDataCallback = function(response) { console.log(response); window.postMessage(response);  }; ' +
+    'var onDataExpiredCallback = function(error) {  window.postMessage("expired"); }; ' +
+    'var onDataErrorCallback = function(error) {  window.postMessage("error"); } </script>' +
+    '</head><body>' +
+    '<div style="text-align: center"><div class="g-recaptcha" style="display: inline-block"' +
+    'data-sitekey="' +
+    siteKey +
+    '" data-callback="onDataCallback" ' +
+    'data-expired-callback="onDataExpiredCallback" ' +
+    'data-error-callback="onDataErrorCallback"></div></div></body></html>';
+  return originalForm;
+};
+
+const RNReCaptcha = ({ onMessage, siteKey, style, url }) => (
+  <WebView
+    originWhitelist={['*']}
+    mixedContentMode={'always'}
+    onMessage={onMessage}
+    javaScriptEnabled
+    injectedJavaScript={patchPostMessageJsCode}
+    automaticallyAdjustContentInsets
+    style={[{ backgroundColor: 'transparent', width: '100%' }, style]}
+    source={{
+      html: generateTheWebViewContent(siteKey),
+      baseUrl: `${url}`,
+    }}
+  />
+);
+
+RNReCaptcha.propTypes = {
+  onMessage: PropTypes.func,
+  siteKey: PropTypes.string.isRequired,
+  style: PropTypes.any,
+  url: PropTypes.string,
+};
+
+RNReCaptcha.defaultProps = {
+  onMessage: () => {},
+  url: '',
+};
+
+export default RNReCaptcha;
